702d3a134c442e39685c7d4dda53dd53fd04367b
mollyfoz <magnificom@aetna.com>
2020-01-30 10:08:44 -0700

    intentionally fail step

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -1,5 +1,5 @@
 Given(/^I am on the homepage$/) do
-  find('h1', :text => "Welcome to MyRapName.com")
+  find('h1', :text => "Welcome to MyRapWebsite.com")
 end
 
 Then(/^I see the text "(.*?)"$/) do |text|
