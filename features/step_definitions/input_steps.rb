a6a13bbe87271f5c1f6a47ae1bdd530dbf14a526
msimon42 <msimon@uab.edu>
2020-06-19 19:46:44 -0600

    Add input tests

--- features/step_definitions/input_steps.rb
+++ features/step_definitions/input_steps.rb
@@ -1,4 +1,31 @@
-Given('I am on the name-generator page') do
-  find('h2', text: 'Name Generator')
-  find('h3', text: 'Find the Perfect Fantasy Name')
+Given(/^I am on the fantasy name page$/) do
+  find('h2', :text => "Name Generator")
+end
+
+And(/^I see the text "(.*?)"$/) do |text|
+  page.has_text?(text)
+end
+
+Then('There are 23 categories and all are selected') do
+  @categories = all('div', class: 'fantasy_checkbox_div', count: 23)
+  @categories.each do |category|
+    category.find_field('fantasy_types[]', checked: true)
+  end
+end
+
+And('I can deselect all categories') do
+  click_button('Uncheck all')
+  @categories.each do |category|
+    category.find_field('fantasy_types[]', checked: false)
+  end
+end
+
+And(/^I can select the category "(.*?)"$/) do |category|
+  check('fantasy_types[]', option: category)
+end
+
+Then('I can generate names for the selected category') do
+  click_button('Write me some fantasy names')
+  find('h1', text: 'Random Fantasy Names')
+  find('h2', text: 'Best Suggestions')
 end
